63ef214d789e5eb0c5a641610ade9449f9638562
MJ Rossetti <s2t2mail+github@gmail.com>
2016-03-17 19:46:24 -0600

    adding routes and views

--- CREDITS.md
+++ CREDITS.md
@@ -3,4 +3,5 @@
   + https://canvas.devpointlabs.com/courses/4/assignments/61?module_item_id=104
   + http://expressjs.com/en/starter/installing.html
   + http://expressjs.com/en/starter/hello-world.html
+  + http://ejs.co/
   + http://expressjs.com/en/starter/basic-routing.html
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Robots App!'
+  });
+});
+
+router.get('/robots', function (req, res) {
+  res.render('robots', {
+    title: 'All Robots'
+  });
+});
+
+router.get('/robots/new', function (req, res) {
+  res.render('robots/new', {
+    title: 'Add a New Robot'
+  });
 });
 
 module.exports = router;
